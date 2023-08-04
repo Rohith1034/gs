ef7e6d9f754d1527dba523497af74f354f32c31a
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2023-08-04 22:30:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+    res.render("index.ejs", {
+        full_name: name,
+    });
+});
 
-app.use(express.static(__dirname));
+app.get("/grocery_account_page.ejs", function (req, res) {
+    res.render("grocery_account_page.ejs");
+});
 
-app.listen(3000,function(req,res) {
+app.listen(3000, function () {
     console.log("Server started");
-})
+});
+
+var name = null;
+
+app.post("/submit", function (req, res) {
+    name = req.body.fullname;
+    const Email = req.body.email;
+    const phoneNo = req.body.phone;
+    const pin_code = req.body.pincode;
+    const Address = req.body.address;
+    console.log(name, Email, phoneNo, pin_code, Address);
+    res.render("index.ejs", {
+        full_name: name,
+    });
+});
